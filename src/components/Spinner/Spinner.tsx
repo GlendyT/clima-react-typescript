41fe4299f5e57710b7be8995f5d91f380534546f
GlendyT <glendytuyuc@gmail.com>
2024-05-03 16:24:09 -0600

    agregar versiones para traer la info de la API, zod, valibot, etc

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import "./Spinne.css"
+
+export default function Spinner() {
+  return (
+    <div>
+        <span className="loader"></span>
+    </div>
+  )
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export const formTemperature = (temperature: number) : number => {
+    const kelvin = 273.15
+    return parseInt((temperature - kelvin).toString())
+}
